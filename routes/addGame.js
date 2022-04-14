7184af8b201f13898389031685d9aa60936d4847
Szymon Bacanski <s.bacanski@gmail.com>
2022-04-14 21:06:54 +0200

    Update addGame.js

--- routes/addGame.js
+++ routes/addGame.js
@@ -108,83 +108,95 @@ router.post('/by-link', async (request, response) => {
 	console.log("Got 'add-game/by-link' order: ", request.body);
 	console.log("header - 'authentication': ", request.header('authentication'));
 
-	const { error } = addingGameByLinkValidation(request.body);
-	if (error) {
-		return response.json(responseStandard(false, error.details[0].message));
-	}
-
-	const token = request.header('authentication');
-	const link = request.body.link.toString();
-
-	const { _id } = jwt.verify(token, process.env.TOKEN_SECRET, (err, decode) => {
-		if (err) {
-			return res.json(responseStandard(false, 'Token Invalid'));
+	try {
+		const { error } = addingGameByLinkValidation(request.body);
+		if (error) {
+			throw error.details[0].message;
 		}
-		return decode;
-	});
 
-	const userFromToken = await User.findById(_id);
-	console.log(userFromToken);
+		if (!request.body.link.includes('https://store.steampowered.com/app/')) {
+			throw 'Link Invalid - pass right link';
+		}
 
-	const gameNumber = link
-		.split('/')
-		.filter(element => element !== '')
-		.at(-2);
+		const token = request.header('authentication');
+		const link = request.body.link.toString();
 
-	const steamUrlSchema =
-		'https://store.steampowered.com/api/appdetails?appids={app_id}&cc=pl';
-
-	const gameFromSteam = await fetch(
-		steamUrlSchema.replace('{app_id}', gameNumber)
-	).then(res => res.json());
-	const name = gameFromSteam[gameNumber].data.name;
-	const steam_appid = gameFromSteam[gameNumber].data.steam_appid;
-	const currency = gameFromSteam[gameNumber].data.price_overview.currency;
-	const initial = gameFromSteam[gameNumber].data.price_overview.initial;
-	const final = gameFromSteam[gameNumber].data.price_overview.final;
-	const discount_percent =
-		gameFromSteam[gameNumber].data.price_overview.discount_percent;
-
-	const isGameInDB = await Game.findOne({
-		name: name,
-		steam_appid: steam_appid,
-	});
-
-	if (isGameInDB) {
-		const gameInDB = await Game.findOne({ steam_appid: steam_appid });
-
-		const hasUserHasThisGame = userFromToken.gamesList.find(
-			element => element.valueOf() === gameInDB._id.valueOf()
+		const { _id } = jwt.verify(
+			token,
+			process.env.TOKEN_SECRET,
+			(err, decode) => {
+				if (err) {
+					throw 'Token Invalid';
+				}
+				return decode;
+			}
 		);
 
-		if (hasUserHasThisGame) {
-			return response.json(responseStandard(false, 'User has this game'));
+		const userFromToken = await User.findById(_id);
+
+		if (!userFromToken) {
+			throw "User doesn't exist";
 		}
 
-		if (!hasUserHasThisGame) {
-			userFromToken.gamesList.push(gameInDB._id);
-			userFromToken.save();
-			return response.json(responseStandard(true, 'Game Added'));
-		}
-	}
+		const gameNumber = link
+			.split('/')
+			.filter(element => element !== '')
+			.at(-2);
 
-	if (!isGameInDB) {
-		const newGame = new Game({
+		const steamUrlSchema =
+			'https://store.steampowered.com/api/appdetails?appids={app_id}&cc=pl';
+
+		const gameFromSteam = await fetch(
+			steamUrlSchema.replace('{app_id}', gameNumber)
+		).then(res => res.json());
+
+		let name = gameFromSteam[gameNumber].data.name;
+		let steam_appid = gameFromSteam[gameNumber].data.steam_appid;
+		let currency = gameFromSteam[gameNumber].data.price_overview.currency;
+		let initial = gameFromSteam[gameNumber].data.price_overview.initial;
+		let final = gameFromSteam[gameNumber].data.price_overview.final;
+		let discount_percent =
+			gameFromSteam[gameNumber].data.price_overview.discount_percent;
+
+		const isGameInDB = await Game.findOne({
 			name: name,
 			steam_appid: steam_appid,
-			currency: currency,
-			priceInitial: initial,
-			priceFinal: final,
-			discountPercent: discount_percent,
 		});
-		try {
+
+		if (isGameInDB) {
+			const gameInDB = await Game.findOne({ steam_appid: steam_appid });
+
+			const hasUserHasThisGame = userFromToken.gamesList.find(
+				element => element.valueOf() === gameInDB._id.valueOf()
+			);
+
+			if (hasUserHasThisGame) {
+				throw 'User has this game';
+			}
+
+			if (!hasUserHasThisGame) {
+				userFromToken.gamesList.push(gameInDB._id);
+				userFromToken.save();
+				return response.json(responseStandard(true, 'Game Added'));
+			}
+		}
+
+		if (!isGameInDB) {
+			const newGame = new Game({
+				name: name,
+				steam_appid: steam_appid,
+				currency: currency,
+				priceInitial: initial,
+				priceFinal: final,
+				discountPercent: discount_percent,
+			});
 			newGame.save();
 			userFromToken.gamesList.push(newGame._id);
 			userFromToken.save();
 			return response.json(responseStandard(true, 'Game Added'));
-		} catch (err) {
-			return response.json(responseStandard(false, err));
 		}
+	} catch (err) {
+		return response.json(responseStandard(false, err));
 	}
 });
 
