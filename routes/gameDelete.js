f802d7808dec45f92971a3d8fd13e8c8cd7b20d9
Szymon Bacanski <s.bacanski@gmail.com>
2022-04-14 14:50:04 +0200

    Update gameDelete.js

--- routes/gameDelete.js
+++ routes/gameDelete.js
@@ -6,21 +6,19 @@ const Game = require('../models/Game.js');
 
 const { gameDeleteValidation } = require('../validation/deleteValidation.js');
 const responseStandard = require('../controller.js');
-const { TokenExpiredError } = require('jsonwebtoken');
-const { findById } = require('../models/User.js');
 
-router.delete('/delete-game', async (req, res) => {
+router.delete('/game/:gameId', async (req, res) => {
 	console.log('---------------------------------');
-	console.log(' Delete Order - delete.game:', req.body);
+	console.log(' Delete Order - req.params:', req.params);
 	console.log("header - 'authentication': ", req.header('authentication'));
 
 	try {
-		const { error } = gameDeleteValidation(req.body);
+		const { error } = gameDeleteValidation(req.params);
 		if (error) {
 			throw error.details[0].message;
 		}
 
-		const gameIdToDelete = req.body.gameId;
+		const gameIdToDelete = req.params.gameId;
 		const token = req.header('authentication');
 
 		const { _id } = jwt.verify(
@@ -33,29 +31,23 @@ router.delete('/delete-game', async (req, res) => {
 				return decode;
 			}
 		);
-
 		const userFromId = await User.findById(_id);
-
 		if (!userFromId) {
 			throw "User doesn't exist";
 		}
-
 		const userGamesObjectId = userFromId.gamesList;
-		const games = await Promise.all(
+		const userGames = await Promise.all(
 			userGamesObjectId.map(async objectId => {
 				const game = await Game.findById(objectId);
 				return game;
 			})
 		);
-
-		const filteredGame = games.find(
-			game => game.steam_appid === gameIdToDelete
+		const filteredGame = userGames.find(
+			game => game.steam_appid == gameIdToDelete
 		);
-
 		if (!filteredGame) {
 			throw "User doesn't have this game";
 		}
-
 		userFromId.gamesList.remove(filteredGame._id);
 		await userFromId.save();
 		return res.json(responseStandard(true, 'Game deleted'));
